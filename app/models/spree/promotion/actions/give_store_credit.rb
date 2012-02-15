f1771fc95baa5d65427b452bd5a9391f71705dcb
Anita <anita.rao.u@gmail.com>
2012-02-15 16:16:46 +0530

    Add optional expiry date to store credit.

--- app/models/spree/promotion/actions/give_store_credit.rb
+++ app/models/spree/promotion/actions/give_store_credit.rb
@@ -2,10 +2,11 @@ module Spree
   class Promotion::Actions::GiveStoreCredit < PromotionAction
     preference :amount, :decimal, :default => 0.0
 
+    preference :expiry_days, :integer, :default => 0
+
     def perform(options = {})
       if user = options[:user]
-        user.store_credits.create(:amount => preferred_amount, :remaining_amount => preferred_amount,  :reason => "Promotion: #{promotion.name}")
-      else
+        user.store_credits.create(:amount => preferred_amount, :remaining_amount => preferred_amount,  :reason => "Promotion: #{promotion.name}", :expires_at => (preferred_expiry_days <= 0 ? :nil : Time.now + preferred_expiry_days.days))
       end
     end
   end
